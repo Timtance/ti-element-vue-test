076fdd879b8b2b2366ee0a85428a11f386738ec3
Tui.txh <tanceboy@163.com>
2024-01-10 13:33:24 +0800

    Add files via upload

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true
+  },
+  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended'],
+  parserOptions: {
+    ecmaVersion: 12,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    'vue/no-unused-vars': 'off',
+    'no-unused-vars': 'off',
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'import/no-absolute-path': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'vue/no-multiple-template-root': 'off',
+    'vue/html-self-closing': 'off',
+    'no-console': 'off',
+    'no-bitwise': 'off',
+    'no-param-reassign': 'off'
+  }
+}
